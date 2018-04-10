de80144479af556dd514858cf31121b845a13f95
Waqaar A <waqaar145@gmail.com>
2018-04-10 11:46:59 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  mongoUrl: "mongodb://localhost:27017/faasos",
-  // mongoUrl: "mongodb://---:----@ds121889.mlab.com:21889/faasos"
+  mongoUrl: "mongodb://localhost:27017/ooo",
+  // mongoUrl: "mongodb://---:----@ds121889.mlab.com:21889/ooo"
 };
